fd13d5b040612280cbf370011f871993ec75bf3d
Li Tiancheng <tli637@aucklanduni.ac.nz>
2026-08-07 23:04:20 +1200

    Version 2.2.22 fixing pdf bugs

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26fb3dc7d73171c3841ef2890d66f11ff6b12f18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72a131dff8c1252a898643bb08097c63d8e29ef8")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
