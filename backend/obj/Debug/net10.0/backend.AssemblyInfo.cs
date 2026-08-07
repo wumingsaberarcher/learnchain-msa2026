72a131dff8c1252a898643bb08097c63d8e29ef8
Li Tiancheng <tli637@aucklanduni.ac.nz>
2026-08-07 22:55:32 +1200

    Version 2.2.20 fixing some bugs

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3417a7067b036708317cc95500531c6a379550bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26fb3dc7d73171c3841ef2890d66f11ff6b12f18")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
